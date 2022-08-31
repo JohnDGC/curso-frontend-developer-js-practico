e1a88c404b1e2e3e0f49b33ad8411459acb32d35
JohnDGC <jonchygomez@yahoo.es>
2022-08-31 11:11:19 -0500

    Mobile menu fusion

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('mouseover', toggleMobileMenu);
 
-function toggleDesktopMenu(event) {
-  console.log(event)
-  event.preventDefault();
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive')
 }
